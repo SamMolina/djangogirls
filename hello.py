06780d777a3711d96900029fdbde37e4bab2c015
SamMolina <smolina@thoughtworks.com>
2017-09-02 16:30:16 -0500

    Mi primer commit

---
+++ hello.py
@@ -0,0 +1,8 @@
+fruits=['apple', 'orange', 'banana']
+
+def fruitsNames(name):
+    print("Hi! " + name)
+    for fruit in fruits:
+        if fruit == 'apple':
+            print('I love apples')
+        print('Hello! I am a ' + fruit)
